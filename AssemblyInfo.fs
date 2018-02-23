7a7f3d9879b767779bc495f6be43c08e78763754
William Yao <william.yao@earnin.com>
2018-02-22 20:32:52 -0800

    add assembly info

---
+++ AssemblyInfo.fs
@@ -0,0 +1,13 @@
+module Renatus.AssemblyInfo
+
+open System
+open System.Reflection
+open System.Runtime.InteropServices
+open System.Runtime.CompilerServices
+
+[<assembly: ComVisible(false)>]
+[<assembly: CLSCompliant(false)>]
+
+[<assembly: InternalsVisibleTo("Renatus.Test")>]
+
+()
